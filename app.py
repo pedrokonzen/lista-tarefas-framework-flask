aa8c820f688bdeab95119dd702b53801c2347f3e
Pedro Konzen Rhoden <pedro.rhoden@aluno.feliz.ifrs.edu.br>
2023-06-08 23:42:45 -0300

    update and delete

--- app.py
+++ app.py
@@ -13,6 +13,22 @@ def add_task():
     task = request.form['task']
     tasks.append(task)
 
+    # Lógica para adicionar a tarefa ao banco de dados ou à lista de tarefas existente
+    return redirect('/')
+
+@app.route('/update_task/<int:task_id>', methods=['POST'])
+def update_task(task_id):
+    task = request.form['task']
+    tasks[task_id] = task
+
+    # Lógica para atualizar a tarefa no banco de dados ou na lista de tarefas existente
+    return redirect('/')
+
+@app.route('/delete_task/<int:task_id>', methods=['GET', 'POST'])
+def delete_task(task_id):
+    del tasks[task_id]
+    return redirect('/')
+
 
 if __name__ == '__main__':
     app.run()
